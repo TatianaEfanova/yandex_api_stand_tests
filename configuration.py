081807cb493de6749d5b48e841d1f1cc2be014b2
Tatiana Efanova <etatiana138@yandex.ru>
2023-09-02 12:49:57 +0400

    API testing - User creation - First Name field

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://934945fe-3242-4f11-9e4e-3305e5bb0635.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
